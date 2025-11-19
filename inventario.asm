64541d3e38f28f4141ead95801d02af5f99e549e
RenaC15 <rencolon62@gmail.com>
2025-11-18 23:50:11 -0400

    espero que no explote tu compu

--- inventario.asm
+++ inventario.asm
@@ -1,18 +1,18 @@
 .data
 #Este es el inventario que utilizara la caja registradora 
-p001:   .word 000001234 320 0 75
+p001:   .word 1234 320 0 75
 	.asciiz "Jugo de naranja"
 	.align 5
-p002:   .word 000005656 1023 1 23
+p002:   .word 5656 1023 1 23
 	.asciiz "Galletas de soda"
 	.align 5
 p003:   .word 100000001 106 2 57
 	.asciiz "Corn Flakes 500g"
 	.align 5
-p004:   .word 000001111 30 5 00
+p004:   .word 1111 30 5 00
 	.asciiz "Leche 0.5L"
 	.align 5
-p005:   .word 000001121 58 0 50
+p005:   .word 1121 58 0 50
 	.asciiz "Queso blanco"
 	.align 5
 	
